47f86e3820742782c40cdb491f16aabbbc4fc557
Enis Spahi <enisspahi@gmail.com>
2017-08-02 00:41:44 +0200

    Implement Facebook oauth client

--- facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
+++ facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
@@ -1,10 +1,12 @@
 package com.my.demos.facebooklogindemo;
 
+import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@EnableOAuth2Sso
 public class FacebookLoginDemoController {
 
     @RequestMapping("/myDemoPage")
