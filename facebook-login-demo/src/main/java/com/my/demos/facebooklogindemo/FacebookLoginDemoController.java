7c85dafcc9784f40b3efdcb876fb3f1e6edf188f
Enis Spahi <enisspahi@gmail.com>
2017-08-02 00:59:09 +0200

    Customize pages to secure

--- facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
+++ facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
@@ -1,18 +1,29 @@
 package com.my.demos.facebooklogindemo;
 
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @EnableOAuth2Sso
-public class FacebookLoginDemoController {
+public class FacebookLoginDemoController extends WebSecurityConfigurerAdapter {
 
     @RequestMapping("/myDemoPage")
     public String myDemoPage(Model model) {
         model.addAttribute("message", "Hello from MVC controller");
         return "myDemoPage";
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .antMatcher("/**").authorizeRequests()
+                .antMatchers("/").permitAll()
+                .anyRequest().authenticated();
+
     }
 
 }
