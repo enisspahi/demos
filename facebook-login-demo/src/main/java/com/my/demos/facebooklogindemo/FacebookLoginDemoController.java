e68effd7457058f51380e7b1b51b9611ace7199f
Enis Spahi <enisspahi@gmail.com>
2017-08-06 15:44:40 +0200

    Show oauth authentication info

--- facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
+++ facebook-login-demo/src/main/java/com/my/demos/facebooklogindemo/FacebookLoginDemoController.java
@@ -3,7 +3,7 @@ package com.my.demos.facebooklogindemo;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.provider.OAuth2Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,10 +13,9 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class FacebookLoginDemoController extends WebSecurityConfigurerAdapter {
 
     @RequestMapping("/myDemoPage")
-    public String myDemoPage(Authentication authentication, Model model) {
+    public String myDemoPage(OAuth2Authentication authentication, Model model) {
         model.addAttribute("message", "Hello from MVC controller");
-        model.addAttribute("userId", authentication.getName());
-        model.addAttribute("details", authentication.getDetails());
+        model.addAttribute("userDetails", authentication.getUserAuthentication().getDetails());
         model.addAttribute("isAuthenticated", authentication.isAuthenticated());
         return "myDemoPage";
     }
@@ -26,7 +25,9 @@ public class FacebookLoginDemoController extends WebSecurityConfigurerAdapter {
         http
                 .antMatcher("/**").authorizeRequests()
                 .antMatchers("/").permitAll()
-                .anyRequest().authenticated();
+                .anyRequest().authenticated()
+                .and().logout();
+
     }
 
 }
