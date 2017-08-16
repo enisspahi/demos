3994a33eab3ea085e8b815112ea2b8a1ad95d17d
Enis Spahi <enisspahi@gmail.com>
2017-08-16 20:39:44 +0200

    create hystrix weather demo project

---
+++ hystrix-weather-demo/src/main/java/com/my/demos/hystrixweatherdemo/HystrixWeatherDemoApplication.java
@@ -0,0 +1,12 @@
+package com.my.demos.hystrixweatherdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HystrixWeatherDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HystrixWeatherDemoApplication.class, args);
+	}
+}
---
+++ hystrix-weather-demo/src/test/java/com/my/demos/hystrixweatherdemo/HystrixWeatherDemoApplicationTests.java
@@ -0,0 +1,16 @@
+package com.my.demos.hystrixweatherdemo;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HystrixWeatherDemoApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
